7bd0237c6dea110cc9484fdba24e579d94b35196
Mike Lofgren <mikeylofgren@gmail.com>
2020-09-13 15:36:03 -0700

    add component structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TripsComponent } from './trips/trips.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { TripDetailComponent } from './trips/trip-detail/trip-detail.component';
+import { ItemListComponent } from './trips/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TripsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    TripDetailComponent,
+    ItemListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/trips/trip-detail/trip-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-detail',
+  templateUrl: './trip-detail.component.html',
+  styleUrls: ['./trip-detail.component.css'],
+})
+export class TripDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
